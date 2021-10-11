914c47fecdd01faf9bdb705283dd1ad6945ece9c
qzs <qzsiniong@163.com>
2021-10-11 10:48:27 +0800

    gh action

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,10 +27,6 @@ const count = ref(0)
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
